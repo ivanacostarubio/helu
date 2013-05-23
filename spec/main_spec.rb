31a5a499fac8e2cf99831de410cfd901cfce7338
Ivan Acosta-Rubio <ivan@bakedweb.net>
2013-05-22 21:01:18 -0500

    retrofitting some tests

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,9 +1,66 @@
+#
+# SKPaymentQueue
+# + defaultQueue
+#   - addTransactionObserver(observer)
+#   - addPayment(product)
+#   - finishTransaction(transaction)
+# SKPayment
+# + paymentWithProductIdentifier(id)
+#
+
+class FakeQueue
+
+  attr_accessor :added_observer, :payment_sent
+
+ def self.instance
+    Dispatch.once { @instance ||= new }
+    @instance
+  end
+
+  def addTransactionObserver(object)
+    @added_observer = true
+  end
+
+  def addPayment(product)
+    @payment_sent = true
+  end
+
+  def finishTransaction(transaction)
+  end
+end
+
+
+class SKPaymentQueue
+  def self.defaultQueue
+    FakeQueue.instance
+  end
+end
+
+
+
+class SKPayment
+
+  def self.paymentWithProductIdentifier(id)
+  end
+end
+
+
 describe "Application 'helu'" do
+
   before do
     @app = UIApplication.sharedApplication
   end
 
-  it "has one window" do
-    @app.windows.size.should == 1
+  it "adds the observer to self in initialization" do 
+    FakeQueue.instance.added_observer.should == nil
+    @carlos = Helu.new("wasa")
+    FakeQueue.instance.added_observer.should == true
+  end
+
+  it "sends the payment to apple" do
+    @carlos = Helu.new("wasa")
+    FakeQueue.instance.payment_sent.should == nil
+    @carlos.buy
+    FakeQueue.instance.payment_sent.should == true
   end
 end
