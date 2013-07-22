906473d58631aa86fc9a6c39b5708abf04f3a3bd
Ivan Acosta-Rubio <ivan@softwarecriollo.com>
2013-07-22 11:49:17 -0500

    retrofitting some test

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -22,7 +22,7 @@ class FakeQueue
   attr_accessor :added_observer, :payment_sent, :restoring
 
  def self.instance
-    Dispatch.once { @instance ||= new }
+    Dispatch.once{ @instance ||= new }
     @instance
   end
 
@@ -42,39 +42,79 @@ class FakeQueue
   end
 end
 
-
 class SKPaymentQueue
   def self.defaultQueue
     FakeQueue.instance
   end
 end
 
-
-
 class SKPayment
-
   def self.paymentWithProductIdentifier(id)
   end
 end
 
+class HeluQueue
 
-describe "public api" do
-
-  before do
-    @app = UIApplication.sharedApplication
+  class Transaction
+    def payment
+      HeluQueue::Payment.new
+    end
   end
 
-  it "adds the observer to self in initialization" do 
-    FakeQueue.instance.added_observer.should == nil
-    @carlos = Helu.new("wasa")
-    FakeQueue.instance.added_observer.should == true
+  class Payment
+    def productIdentifier
+      "ibw"
+    end
   end
+
+  def transactions
+    [HeluQueue::Transaction.new]
+  end
+end
+
+describe "Interacting with Apple" do
 
   it "sends the request to apple" do
     @carlos = Helu.new("wasa")
     FakeQueue.instance.payment_sent.should == nil
     @carlos.buy
     FakeQueue.instance.payment_sent.should == true
+  end
+
+  it "adds the observer to self in initialization" do 
+    FakeQueue.instance.added_observer = nil
+    FakeQueue.instance.added_observer.should == nil
+    @carlos = Helu.new("wasa")
+    FakeQueue.instance.added_observer.should == true
+  end
+end
+
+describe "Public API" do
+
+  before do
+    @app = UIApplication.sharedApplication
+  end
+
+  it "executes the failed block for a failed transaction" do
+    @carlos = Helu.new("ibw")
+    @carlos.fail = lambda { |t| @@failed = true}
+    @carlos.finishTransaction(Transaction.new, wasSuccessful:false)
+    @@failed.should == true
+  end
+
+  it "executes the winning block for a successful transaction" do 
+    @carlos = Helu.new("ibw")
+    @carlos.winning = lambda {|t| @@successful = true}
+    @carlos.finishTransaction(Transaction.new, wasSuccessful:true)
+    @@successful.should == true
+  end
+
+  it "executes the restore block if a product was already bought" do 
+    @carlos = Helu.new("ibw")
+    @carlos.restore = lambda { @@restore = true }
+    queue = HeluQueue.new
+    @carlos.paymentQueueRestoreCompletedTransactionsFinished(queue)
+    @@restore.should == true
   end
 
   it "can restore a transaction" do 
@@ -85,7 +125,7 @@ describe "public api" do
     FakeQueue.instance.restoring.should == true
   end
   
-  it "records the purchase on the locally" do 
+  it "records the purchase locally" do 
     @carlos = Helu.new("ibw")
     @carlos.buy
     @carlos.winning = lambda { |t|  }
@@ -102,7 +142,6 @@ describe "public api" do
 end
 
 describe "Helu::LocalStore" do 
-
   it "adds for the first time to the local store" do 
     Helu::LocalStorage.new.clean
     Helu::LocalStorage.new.all.should == []
@@ -116,5 +155,4 @@ describe "Helu::LocalStore" do
     Helu::LocalStorage.new.add("insight_10")
     Helu::LocalStorage.new.all.should == ["ibw", "insight_10"]
   end
-
 end
